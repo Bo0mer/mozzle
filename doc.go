c9f30152f89aa2e17677ff8997466edc57c76c42
Ivan Borshukov <bo0merzzz@gmail.com>
2017-01-17 11:42:28 +0200

    mozzle: Fix godoc typos

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Package mozzle implements Cloud Foundry applicaiton monitor that emits
+// Package mozzle implements Cloud Foundry application monitor that emits
 // metric events.
 //
 // Events for the following metrics are emitted.
@@ -25,6 +25,6 @@
 // Each of the events has attributes specifying the application's
 // org, space, name, and the insntace index (when appropriate).
 //
-// Additionally, the HTTP events has attributes specifiying the method,
+// Additionally, the HTTP events has attributes specifying the method,
 // request_id, content length and the returned status code.
 package mozzle
